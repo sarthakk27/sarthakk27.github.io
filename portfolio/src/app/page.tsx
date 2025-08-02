8b44f1ea0cbf640cf3775a1deac65cac811911ea
sarthak <sarthakrathor27@gmail.com>
2025-08-02 13:40:55 +0530

    added components

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+import FlipLink from '@/components/ui/text-effect-flipper';
+import ThemeToggleButton from '@/components/ui/theme-toggle-button';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <ThemeToggleButton />
+      hello soahsjispdhsodhi dhsipdhidjposdpiajhd piahdpasihdpsjdahjpa asihjajhdpoajpsj
+      <FlipLink href="https://www.linkedin.com/in/rathor-sarthak-b76277357/">
+          LINKEDIN
+      </FlipLink>
+      <FlipLink href="">I</FlipLink> <br />
+      <FlipLink href="">N</FlipLink>
+      
+    </div>
+  );
+}
+
+export default page;
